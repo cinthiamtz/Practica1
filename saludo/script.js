413adbfa077b629fbddbe44df60e5d4311c83a55
cinthia martinez <cinthia.martinez.mtz@outlook.com>
2022-06-17 15:10:27 -0500

    Se realiza el cambio dinamico de h2

--- saludo/script.js
+++ saludo/script.js
@@ -1,1 +1,18 @@
-console.log (" Se nota que todavia me extrañas ");
+console.log(" Se nota que todavia me extrañas ");
+
+setInterval(cambiarSaludo, 2000);
+
+function cambiarSaludo() {
+  //console.log("Me parece una falta de respecto");
+  //Estoy asignando 
+  let frase = document.getElementsByTagName("h2")[1].innerHTML;
+
+  if (document.getElementsByTagName("h2")[1].innerHTML == "Ganando como siempre") {
+    //frase = "Impongo moda para las niñas!";
+    document.getElementsByTagName("h2")[1].innerHTML = "Impongo moda para las niñas!";
+  } else {
+    //frase = "Ganando como siempre";
+    document.getElementsByTagName("h2")[1].innerHTML = "Ganando como siempre";
+  }
+  console.log("frase :" + frase);
+}
