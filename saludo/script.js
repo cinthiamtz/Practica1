29da4b5dd9ae7b6862b5a6063d6eb088d276ca31
cinthia martinez <cinthia.martinez.mtz@outlook.com>
2022-06-17 15:13:44 -0500

    Se uso innerHTML

--- saludo/script.js
+++ saludo/script.js
@@ -5,14 +5,14 @@ setInterval(cambiarSaludo, 2000);
 function cambiarSaludo() {
   //console.log("Me parece una falta de respecto");
   //Estoy asignando 
-  let frase = document.getElementsByTagName("h2")[1].innerHTML;
-
-  if (document.getElementsByTagName("h2")[1].innerHTML == "Ganando como siempre") {
+  let frase = document.getElementsByTagName("h2")[1];
+    console.log(frase)
+  if (frase.innerHTML == "Ganando como siempre") {
     //frase = "Impongo moda para las niñas!";
-    document.getElementsByTagName("h2")[1].innerHTML = "Impongo moda para las niñas!";
+    frase.innerHTML = "Impongo moda para las niñas!";
   } else {
     //frase = "Ganando como siempre";
-    document.getElementsByTagName("h2")[1].innerHTML = "Ganando como siempre";
+   frase.innerHTML = "Ganando como siempre";
   }
   console.log("frase :" + frase);
 }
