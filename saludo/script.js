ea6c27f5be93b55197cde22eb10df51bf9eece5a
cinthia martinez <cinthia.martinez.mtz@outlook.com>
2022-06-17 15:17:28 -0500

    Se utiliza innerText para generar cambio h2

--- saludo/script.js
+++ saludo/script.js
@@ -2,17 +2,20 @@ console.log(" Se nota que todavia me extrañas ");
 
 setInterval(cambiarSaludo, 2000);
 
+/**
+ * Cambia la frase en el titulo h2
+ */
+
 function cambiarSaludo() {
-  //console.log("Me parece una falta de respecto");
-  //Estoy asignando 
-  let frase = document.getElementsByTagName("h2")[1];
+    //console.log("Me parece una falta de respecto");
+    //Estoy asignando 
+    let frase = document.getElementsByTagName("h2")[1];
     console.log(frase)
-  if (frase.innerHTML == "Ganando como siempre") {
-    //frase = "Impongo moda para las niñas!";
-    frase.innerHTML = "Impongo moda para las niñas!";
-  } else {
-    //frase = "Ganando como siempre";
-   frase.innerHTML = "Ganando como siempre";
-  }
-  console.log("frase :" + frase);
+    if (frase.innerText == "Ganando como siempre") {
+        frase.innerText = "Impongo moda para las niñas!";
+    } else {
+
+        frase.innerText = "Ganando como siempre";
+    }
+    // console.log("frase :" + frase);
 }
